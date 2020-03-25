9803856804ec34c06063bcfd7ceab5123ba4f7b8
zygzagZ <pawelek987@gmail.com>
2020-03-25 22:52:24 +0100

    Przeniesiony katalog vendor

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,63 @@
-"use strict";
+'use strict'
 
 // Load plugins
-const browsersync = require("browser-sync").create();
-const del = require("del");
-const gulp = require("gulp");
-const merge = require("merge-stream");
+const browsersync = require('browser-sync').create()
+const del = require('del')
+const gulp = require('gulp')
+const merge = require('merge-stream')
 
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: "./"
+      baseDir: './'
     },
     port: 3000
-  });
-  done();
+  })
+  done()
 }
 
 // BrowserSync reload
 function browserSyncReload(done) {
-  browsersync.reload();
-  done();
+  browsersync.reload()
+  done()
 }
 
 // Clean vendor
 function clean() {
-  return del(["./vendor/"]);
+  return del(['./app/vendor/'])
 }
 
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
   // Bootstrap
   var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
+    .pipe(gulp.dest('./app/vendor/bootstrap'))
+
   // jQuery
   var jquery = gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'));
-  return merge(bootstrap, jquery);
+    './node_modules/jquery/dist/*',
+    '!./node_modules/jquery/dist/core.js'
+  ])
+    .pipe(gulp.dest('./app/vendor/jquery'))
+
+  return merge(bootstrap, jquery)
 }
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./**/*.css", browserSyncReload);
-  gulp.watch("./**/*.html", browserSyncReload);
+  gulp.watch('./**/*.css', browserSyncReload)
+  gulp.watch('./**/*.html', browserSyncReload)
 }
 
 // Define complex tasks
-const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor);
-const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+const vendor = gulp.series(clean, modules)
+const build = gulp.series(vendor)
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync))
 
 // Export tasks
-exports.clean = clean;
-exports.vendor = vendor;
-exports.build = build;
-exports.watch = watch;
-exports.default = build;
+exports.clean = clean
+exports.vendor = vendor
+exports.build = build
+exports.watch = watch
+exports.default = build
